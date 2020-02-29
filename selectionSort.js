f101d9dd664d470fe31b0a40c7050c41742f8acd
firshta2016 <firshtashefa@gmail.com>
2020-02-28 16:30:25 -0800

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let temp = 0
+        let left = i
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[left]) {
+                left = j
+            }
+        }
+        temp = arr[i]
+        arr[i] = arr[left]
+        arr[left] = temp
+    }
+    return arr
+}
+var arr = [3,2,10,1000, 900, 2, 1]
+console.log(selectionSort(arr));
