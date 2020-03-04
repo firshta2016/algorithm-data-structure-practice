92e7b474ed44c8f30f6b2cb417acd5dec0ef84bf
firshta2016 <firshtashefa@gmail.com>
2020-03-04 12:40:48 -0800

    mergeSort practice

---
+++ mergeSort.js
@@ -0,0 +1,21 @@
+function mergeSort(arr) {
+    if(arr.length < 2) {
+        return arr
+    }
+
+    let mid = Math.floor(arr.length / 2)
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+
+    return merge(left, right)
+}
+
+function merge (left, right) {
+    let result = []
+    while(left.length > 0 && right.length > 0) 
+    result.push(left[0] < right[0] ? left.shift() : right.shift())
+    return result.concat(left.length ? left : right)
+}
+
+var result = [3,2,10,1000, 900, 2, 1, 88, 4, 29]
+console.log(mergeSort(result));
