e876e7868779fcfe2f8c987d6fa0ca1ab4bb7eb1
firshta2016 <firshtashefa@gmail.com>
2020-03-12 14:13:57 -0700

    added linearSearch algo

---
+++ linearSearch.js
@@ -0,0 +1,11 @@
+function linearSearch(arr, value) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === value) {
+            return i
+        }
+    }
+    return false
+}
+
+let result = [2,5,8,7,9,100]
+console.log(linearSearch(result, 8))
